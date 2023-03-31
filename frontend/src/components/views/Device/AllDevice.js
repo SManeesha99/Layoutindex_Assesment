f84f1a421016222b3e53ce6f63c1c6323dff2f1b
SManeesha99 <it20649012@my.sliit.lk>
2023-03-31 19:18:29 +0530

    All Devices view
    create all devices view

---
+++ frontend/src/components/views/Device/AllDevice.js
@@ -0,0 +1,123 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+
+const AllDevice = () => {
+
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const [devices, setDevices] = React.useState([]);
+    useEffect(()=>{
+        const getAllDevices = async () => {
+          await axios.get(`http://localhost:8090/api/device/devices`).then((res) => {
+            setDevices(res.data.data);
+          console.log( res.data)
+          }).catch((err) => {
+              console.log(err.massage);
+          })
+      }
+        getAllDevices();
+      },[])
+
+    // const [searchTerm, setSearchTerm] = React.useState("");
+    // const [devices, setDevices] = React.useState([]);
+    // useEffect(() => {
+    //     const getAllDevices = async () => {
+    //         await axios.get(`http://localhost:8090/api/device/devices`).then((res) => {
+    //             setDevices(res.data);
+    //             console.log(res.data);
+    //         }).catch((err) => {
+    //             console.log(err);
+    //         })
+    //     }
+    //     getAllDevices();
+    // },[]);
+    
+
+    const filteredDevices = devices.filter((devices) => {
+        return (
+            devices.serialNo.toLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
+            devices.type.toLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
+            devices.locationName.toLowerCase().includes(searchTerm.toLocaleLowerCase()) ||
+            devices.status.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+        );
+    });
+
+    
+
+  return (
+    <div>
+
+        <h3 className="mb-4 pb-2 pb-md-0 mb-md-5">Devices List</h3>
+            <br />
+            {/* <div className="container">
+                <form class="form-inline my-2 my-lg-0">
+                    <div className="row ">
+                        <input class="form-control mr-sm-2 inputSearch" type="text" placeholder='Enter the location or device name ' onChange={(e) => setSearchTerm(e.target.value)} />&nbsp;
+                        <a type="a" class="btn btn-primary inputSearch" href="#">add device</a>
+                    </div>
+                </form>
+            </div> */}
+            <br/>
+            <div class="row row-cols-1 row-cols-md-5 g-5 " style={{ margin:'10px' }}>
+                {filteredDevices.map((devices)=>
+                    <div class="col">
+                        <div class="card">
+                        <img class="card-img-top" src={devices.photo} alt="Card image cap" />
+                        <div class="card-body">
+                            
+                            <h5 class="card-title">{devices.serialNo}</h5>
+                                <p class="card-text">{devices.type}</p>
+                                <p class="card-text">{devices.locationName}</p>
+                                <p class="card-text">{devices.status}</p>
+                                <div className='row'>
+                                    <div className='btn-group'>
+                                        <a href='#' class="btn btn-success">Edit</a>&nbsp;
+                                        <button type="button" class="btn btn-danger">Delete</button>
+                                    </div>
+
+                                </div>
+                        </div>
+                        </div>
+                    </div>
+                    
+                )}
+                
+            </div>
+            {/* <div className='row row-cols-1 row-cols-md-2 g-4'>
+                {filteredDevices.map((devices) => (
+                    <div className='col-md-3 mb-4 pb-4'>
+                        <div class="card" style={{ width: "18rem" }}>
+                            <img class="card-img-top" src={devices.photo} alt="Card image cap" />
+                            <div class="card-body">
+                                <h5 class="card-title">{devices.serialNo}</h5>
+                                <p class="card-text">{devices.type}</p>
+                                <p class="card-text">{devices.locationName}</p>
+                                <p class="card-text">{devices.status}</p>
+                                <div className='row'>
+                                    <div className='col-md-3 mb-4 pb-4'>
+                                        <a href='#' class="btn btn-warning">Edit</a>&nbsp;
+
+                                    </div>
+                                    <div className='col-md-3 mb-4 pb-4'>
+                                    </div>
+                                    <div className='col-md-3 mb-4 pb-4'>
+                                           <button type="button" class="btn btn-danger">Delete</button>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                ))}
+
+            </div> */}
+
+
+
+    </div>
+  )
+}
+
+
+export default AllDevice
